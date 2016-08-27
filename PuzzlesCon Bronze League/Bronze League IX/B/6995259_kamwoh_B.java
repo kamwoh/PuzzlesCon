f8b0e75209b6931e7ec30fd2ce3475d43f9f4fe5
kamwoh <kamwoh@gmail.com>
2016-08-27 19:20:32 +0800

    Update 6995259_kamwoh_B.java

--- PuzzlesCon Bronze League/Bronze League IX/B/6995259_kamwoh_B.java
+++ PuzzlesCon Bronze League/Bronze League IX/B/6995259_kamwoh_B.java
@@ -3,37 +3,17 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main{
-/**
- * 2620
-10
-2
-2530
-461
-1658
-297
-1929
-1923
-152
-463
-2202
- * @param args
- * @throws IOException 
- */
+
     public static void main(String[] args) throws IOException
     {
         Scanner read = new Scanner(System.in);
-//        Scanner read = new Scanner(new FileInputStream("C:\\Users\\pc\\Dropbox (Siswa)\\Dropbox\\Nixie Team Folder\\wohcode\\UVA\\src\\bronze\\input.txt"));
         int testcase = read.nextInt();
-//        int testcase = 1;
         for (int case_ = 0; case_ < testcase; case_++)
         {
             int n = read.nextInt(); //number of days
             int p = read.nextInt(); //number of parties
-//            int n = 2620;
-//            int p = 10;
             boolean[] hartalDays = new boolean[n];
             int[] h = new int[p];
-//            int[] h = {2,2530,461,1658,297,1929,1923,152,463,2202};
             for (int i = 0; i < h.length; i++)
             {
                 h[i] = read.nextInt();
